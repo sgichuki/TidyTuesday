eafed3d32f777c1cd720014e776aa2efe48821b4
sgichuki <53834942+sgichuki@users.noreply.github.com>
2021-09-17 00:13:43 +0200

    Update billboard.R

--- 2021-09-14/billboard.R
+++ 2021-09-14/billboard.R
@@ -66,7 +66,7 @@ ggplot(df1,aes(x = loudness, y = tempo, colour = performer)) +
   annotate("text", x = -13.8, y = 60, label = long_annotation, fontface = 'italic', fill = "white")+
   coord_cartesian(ylim = c(50, 210), clip = "off") +
   theme_minimal() +
-  labs(title = "Loudness vs tempo: Destiny's child, Beyoncé & Kelly Rowland music", x = "dB(decibels)", y = "tempo (beats per minute-BPM)")+
+  labs(title = "Loudness vs tempo: Destiny's child, BeyoncÃ© & Kelly Rowland music", x = "dB(decibels)", y = "tempo (beats per minute-BPM)")+
   theme(plot.title = element_text(color = "#6D006E", hjust = 0.5))+
   theme(legend.position = "bottom")+
   theme(legend.title=element_blank())+
@@ -75,6 +75,48 @@ ggplot(df1,aes(x = loudness, y = tempo, colour = performer)) +
   
 #Save the plot
 ggsave("destinymusic.png",width = 297,height = 210,units = c("mm"),dpi = 300)
+
+#The idea to explore this came at the last minute:
+#Valence - the measure of positivity of the music:
+#A measure from 0.0 to 1.0 describing the musical positiveness 
+#conveyed by a track. Tracks with high valence sound 
+#more positive (e.g. happy, cheerful, euphoric), 
+#while tracks with low valence sound more negative (e.g. sad, depressed, angry).
+
+#Create dataframe with all music data, plus Spotify audio features 
+dfvalence = b1 %>% left_join(f1, by = "song_id")
+
+#create dataframe of songs, year, and valence score
+positivity <- dfvalence %>%
+  distinct(song_id,year,valence) %>%
+  group_by(song_id,valence) %>%
+  filter(row_number() == 1)
+
+#remove commas using gsub in figures; convert valence col to numeric
+positivity$valence <-(gsub(",", ".", positivity$valence))
+positivity$valence <- as.numeric(positivity$valence)
+
+#exclude NA values from data 
+positivity <-na.omit(positivity)
+
+p1 <- positivity %>%
+  group_by(factor(year)) %>%
+  summarise(median(valence))
+
+ggplot(positivity,aes(x = factor(year), y = valence))+
+  geom_boxplot(outlier.colour = "red")+
+  scale_x_discrete(breaks = scales::pretty_breaks())+
+  labs(title = "Are songs on the Billboard Hot 100 becoming more melancholic?",
+       subtitle = "high valence means more positivity, low valence tracks sound more negative(sad, angry etc)", x = " ",
+       caption = "Data:Data.World by way of Sean Miller, Billboard.com and Spotify.viz: Susan G, twitter:@swarau\nTidy Tuesday: Sep 14 2021")+
+  theme_dark()+
+  theme(plot.title = element_text(color = "#008B86"))+
+  theme(plot.background = element_rect(fill = "#E8E6F6"))
+
+#Save the plot
+ggsave("valenceplot.png",width = 297,height = 210,units = c("mm"),dpi = 300)
+
+  
 
 
   
